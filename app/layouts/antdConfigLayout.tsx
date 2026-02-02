a6a62b733b8470909e907fe28973e4d451e6a19a
parham <p.77.pazargadi@gmail.com>
2026-02-02 20:58:43 +0330

    login page completed

--- app/layouts/antdConfigLayout.tsx
+++ app/layouts/antdConfigLayout.tsx
@@ -1,19 +1,21 @@
-"use client"
-import { ConfigProvider } from 'antd'
-import  { ReactNode } from 'react'
+// ./layouts/antdConfigLayout.tsx
+"use client";
 
-const AntdConfigLayout = ({children}:{children:ReactNode}) => {
+import { ConfigProvider } from 'antd';
+import { ReactNode } from 'react';
+
+const AntdConfigLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <ConfigProvider theme={{token:{colorPrimary:""}}}>
-    <html lang="en">
-          <body>
-          <header>Header</header>
-           <div>{children}</div> 
-            <footer>Footer</footer>
-          </body>
-        </html>
-        </ConfigProvider>
-  )
-}
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#1677ff'
+        },
+      }}
+    >
+      {children}
+    </ConfigProvider>
+  );
+};
 
-export default AntdConfigLayout
+export default AntdConfigLayout;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,32 @@
+"use client"
 
-
-import React from 'react'
+import { Button, Form } from 'antd'
+import Link from 'next/link'
 
 const Login = () => {
+  function onFinish(values:LoginInfo) {
+    console.log(values)
+  }
   return (
-    <div>Login</div>
+    <div className=' flex justify-center h-screen  items-center bg-primary'>
+      <div className=' card p-5 w-400 bg-white'>
+        <h1 className=' text-xl'>Portal Jobd - Login</h1>
+        <hr />
+        <Form layout="vertical" className=' flex flex-col gap-3' onFinish={onFinish}>
+          <Form.Item label="Email" name="email">
+            <input  type='email' className='input' />
+
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <input  type='password' className='input' />
+
+          </Form.Item>
+          <p >don't have an account? <Link href="/register" >sign up now</Link></p>
+          <Button type='primary' htmlType="submit">Login</Button>
+        </Form>
+      </div>
+        </div>
   )
 }
 
-export default Login
+ export default Login
